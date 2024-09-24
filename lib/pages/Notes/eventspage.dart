00383fe98db5406addbadb0dce1088dd1a70ba06
kabilan-7-v <kabilanvelmani@gmail.com>
2024-09-24 15:39:05 +0530

    changes

--- lib/pages/Notes/eventspage.dart
+++ lib/pages/Notes/eventspage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Eventspage extends StatelessWidget {
+class Eventspage extends StatefulWidget {
   const Eventspage({super.key});
   static List ongoingevent = [
     "assets/imgs/Rectangle 3904.png",
@@ -14,6 +14,13 @@ class Eventspage extends StatelessWidget {
     "assets/imgs/Rectangle 3904 (6).png",
     "assets/imgs/Rectangle 3904.png",
   ];
+
+  @override
+  State<Eventspage> createState() => _EventspageState();
+}
+
+class _EventspageState extends State<Eventspage> {
+  double height = 225;
 
   @override
   Widget build(BuildContext context) {
@@ -51,101 +58,106 @@ class Eventspage extends StatelessWidget {
   Widget onGoingEvents(BuildContext context) {
     return ListView.builder(
         padding: EdgeInsets.zero,
-        itemCount: ongoingevent.length,
+        itemCount: Eventspage.ongoingevent.length,
         shrinkWrap: true,
         itemBuilder: (context, ind) {
-          return smallcardnotexpand(context, ongoingevent[ind]);
+          return smallcardnotexpand(context, Eventspage.ongoingevent[ind]);
         });
   }
 
   Widget upcomingEvents(BuildContext context) {
     return ListView.builder(
         padding: EdgeInsets.zero,
-        itemCount: upComingevent.length,
+        itemCount: Eventspage.upComingevent.length,
         shrinkWrap: true,
         itemBuilder: (context, ind) {
-          return smallcardnotexpand(context, upComingevent[ind]);
+          return smallcardnotexpand(context, Eventspage.upComingevent[ind]);
         });
   }
 
   Widget smallcardnotexpand(BuildContext context, img) {
-    return Container(
-      height: 225,
-      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Stack(
-            children: [
-              Image.asset(
-                width: double.infinity,
-                img,
-                fit: BoxFit.cover,
-              ),
-              Positioned(
-                right: 10,
-                top: 5,
-                child: Container(
-                  height: 25,
-                  width: 25,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                  ),
-                  child: const Center(child: Icon(Icons.arrow_drop_down)),
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: height,
+        margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Image.asset(
+                  width: double.infinity,
+                  img,
+                  fit: BoxFit.cover,
                 ),
-              )
-            ],
-          ),
-          const Row(
-            children: [
-              SizedBox(
-                width: 20,
-              ),
-              Text("22 July 2024"),
-            ],
-          ),
-          Row(
-            children: [
-              const SizedBox(
-                width: 20,
-              ),
-              const Text(
-                "Birds Day",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              const Spacer(),
-              ConstrainedBox(
-                constraints: const BoxConstraints(maxHeight: 30, maxWidth: 110),
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromRGBO(225, 104, 17, 1)),
-                    onPressed: () {},
-                    child: const Center(
-                        child: Text(
-                      "Join Now",
-                      style: TextStyle(color: Colors.white),
-                    ))),
-              ),
-              const SizedBox(
-                width: 20,
-              )
-            ],
-          ),
-          const Row(
-            children: [
-              SizedBox(
-                width: 20,
-              ),
-              Icon(Icons.location_on_outlined),
-              Text("Adilaide, 33176"),
-            ],
-          )
-        ],
+                Positioned(
+                  right: 10,
+                  top: 5,
+                  child: Container(
+                    height: 25,
+                    width: 25,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                    child: const Center(child: Icon(Icons.arrow_drop_down)),
+                  ),
+                )
+              ],
+            ),
+            const Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Text("22 July 2024"),
+              ],
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 20,
+                ),
+                const Text(
+                  "Birds Day",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const Spacer(),
+                ConstrainedBox(
+                  constraints:
+                      const BoxConstraints(maxHeight: 30, maxWidth: 110),
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromRGBO(225, 104, 17, 1)),
+                      onPressed: () {},
+                      child: const Center(
+                          child: Text(
+                        "Join Now",
+                        style: TextStyle(color: Colors.white),
+                      ))),
+                ),
+                const SizedBox(
+                  width: 20,
+                )
+              ],
+            ),
+            const Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Icon(Icons.location_on_outlined),
+                Text("Adilaide, 33176"),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
